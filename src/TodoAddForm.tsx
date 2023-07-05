1f998976644d068826838d998d5aab13581c93f1
eunseo <eunseolovely@gmail.com>
2023-07-05 12:06:27 +0900

    #6 항목 추가시 유효성 검사

--- src/TodoAddForm.tsx
+++ src/TodoAddForm.tsx
@@ -1,27 +1,41 @@
 import React, { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 interface Props {
   onSubmit(title: string): void
 }
 
-const TodoAddForm = ({ onSubmit }: Props) => {
-  const [inputText, setInputText] = useState('')
+interface IFormData {
+  title: string
+}
 
-  const inputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(e.target.value)
+const TodoAddForm = ({ onSubmit }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>()
+
+  const onAction = (data: any) => {
+    onSubmit(data.title)
   }
 
   return (
     <>
       <h3 className="popup-title">추가하기</h3>
-      <form onSubmit={() => onSubmit(inputText)}>
+      <form onSubmit={handleSubmit(onAction)}>
         <input
-          type="text"
           className="popup-input"
-          placeholder="내용을 입력해주세요."
-          value={inputText}
-          onChange={(e) => inputTextChange(e)}
+          {...register('title', {
+            required: '내용을 입력해주세요',
+            minLength: {
+              value: 10,
+              message: '10자이상 입력해주세요',
+            },
+          })}
+          placeholder="내용"
         />
+        <span className="error-msg">{errors?.title?.message}</span>
         <button type="submit" className="btn-type-01 green">
           저장
         </button>
